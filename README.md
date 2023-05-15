558802df236f5bbaeb5064c4ba5778bcc19b4cdf
OneRain2333 <onerain233@outlook.com>
2023-05-15 11:34:17 +0800

    update poe

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # gpt4free-endpoint
-This is a simple endpoint for the [gpt4free](https://github.com/xtekky/gpt4free) project. 
+
+This is a simple endpoint for the [gpt4free](https://github.com/xtekky/gpt4free) project.
 
 It is a simple Flask app that takes a prompt and returns a response from the GPT-3 API.
 
+Thanks to [poe-openai-proxy](https://github.com/juzeon/poe-openai-proxy) for some of the code.
+
 ## Setup
+
 1. Clone this repo
 2. Install dependencies with `pip install -r requirements.txt`
 3. Run the app with `python app.py`
 
 ## Usage
+
 Refer to OpenAI's [documentation](https://beta.openai.com/docs/api-reference/create-completion) for more information on the parameters.
--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from gpt4free import you
+from gpt4free import you, forefront, theb
 import toml
 import os
 import sys
-from flask import Flask, request
+from flask import Flask, Response, request, stream_with_context
 from flask_sock import Sock
 # CORS
 from flask_cors import CORS
@@ -11,6 +11,7 @@ import random
 import datetime
 import re
 import codecs
+import poe
 
 app = Flask(__name__)
 sock = Sock(app)
@@ -24,6 +25,11 @@ CORS(app, resources={
 })
 sock.init_app(app)
 client_dict = {}
+
+config = toml.load('config.toml')
+print(config)
+
+poe_client = poe.Client(config['poe_token'][0])
 
 def decode_unicode(text):
     pattern = re.compile(r'\\u([0-9a-fA-F]{4})')
@@ -51,9 +57,73 @@ def process_context(messages):
             chat.append({"question": messages[i]['content'], "answer": messages[i+1]['content']})
     return chat
 
+def get_answer(prompt):
+    for token in theb.Completion.create(
+        prompt=prompt,
+    ):
+        yield token
+
+def get_content_to_send(messages):
+    leading_map = {
+        "system": "Instructions",
+        "user": "User",
+        "assistant": "Assistant"
+    }
+    content = ""
+    simulate_roles = False
+    simulate_roles_conf = 2
+    if simulate_roles_conf == 1:
+        simulate_roles = True
+    elif simulate_roles_conf == 2:
+        if (len(messages) == 1 and messages[0]['role'] == "user") or \
+                (len(messages) == 1 and messages[0]['role'] == "system") or \
+                (len(messages) == 2 and messages[0]['role'] == "system" and messages[1]['role'] == "user"):
+            simulate_roles = False
+        else:
+            simulate_roles = True
+    for message in messages:
+        if simulate_roles:
+            content += "||>" + leading_map[message["role"]] + ":\n" + message["content"] + "\n"
+        else:
+            content += message["Content"] + "\n"
+    if simulate_roles:
+        content += "||>Assistant:\n"
+    return content
 
 
-@app.route('/v1/chat/completions', methods=['GET', 'POST', 'OPTIONS'])
+@app.route('/v1/chat/poe', methods=['GET', 'POST', 'OPTIONS'])
+def test_chat():
+    if request.method == 'OPTIONS':
+        print('OPTIONS')
+        return 'ok'
+    content = get_content_to_send(request.json['messages'])
+    def stream():
+        for chunk in poe_client.send_message("capybara", content, with_chat_break=True):
+            print(chunk)
+            text = chunk["text_new"]
+            if text:
+                print("data: %s\n\n" % text.replace("\n","<br>"))
+                dic = {
+                    "choices":[
+                        {
+                            "index":0,
+                            "delta": {
+                                "role": "assistant",
+                                "content": text.replace("\n","<br>")
+                            }
+                        }
+                    ],
+                    "created":str(datetime.datetime.now().timestamp()),
+                    "id":"chatcmpl-" + random.randbytes(8).hex(),
+                    "model":"gpt-3.5-turbo-0301",
+                    "object":"chat.completion.chunk",
+                }
+                yield "data: %s\n\n" % json.dumps(dic, ensure_ascii=False)
+            else:
+                yield "[DONE]"
+    return Response(stream(),mimetype="text/event-stream")
+
+@app.route('/v1/chat/you', methods=['GET', 'POST', 'OPTIONS'])
 def chat():
     if request.method == 'OPTIONS':
         print('OPTIONS')
@@ -65,7 +135,6 @@ def chat():
     )
 
     return convert_json_form(response.text)
-
 
 
 
